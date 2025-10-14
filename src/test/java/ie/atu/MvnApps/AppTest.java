3af2f09ba4f3615908c6ba507eb463a12cfc785e
abbiec123456 <l00196596@atu.ie>
2025-10-14 22:42:32 +0100

    changes

---
+++ src/test/java/ie/atu/MvnApps/AppTest.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+public class AppTest 
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue(true);
+    }
+
+    @Test
+    public void shouldAnswerWithFasle()
+    {
+        assertFalse(false);
+    }
+}
