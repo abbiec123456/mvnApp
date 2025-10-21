4ae3a7117e5330cc9ecb1fb97e301ecb149676ee
abbiec123456 <l00196596@atu.ie>
2025-10-21 19:55:29 +0100

    d

--- src/main/java/ie/atu/MvnApps/App.java
+++ src/main/java/ie/atu/MvnApps/App.java
@@ -5,8 +5,10 @@ package ie.atu.MvnApps;
  * Hello world!
  *
  */
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
     }
 }
